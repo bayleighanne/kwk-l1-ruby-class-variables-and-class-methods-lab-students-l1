cd6d0aa2e5c2cb3fb90510bb1db2ac0b46df4ff4
bayleighanne <bayhaf718@gmail.com>
2018-06-08 20:36:32 +0000

    automatically backed up by learn

--- login.rb
+++ login.rb
@@ -1,14 +1,14 @@
-class User 
+# class User 
   
-  def name=(username)
-    @the_username = username
-  end
+#   def name=(username)
+#     @the_username = username
+#   end
   
-  def name 
-    @the_username
-  end 
-end 
+#   def name 
+#     @the_username
+#   end 
+# end 
 
-jessica = User.new
-jessica.name = "Jessica"
-puts jessica.name 
+# jessica = User.new
+# jessica.name = "Jessica"
+# puts jessica.name 
